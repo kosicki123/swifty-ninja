b4e1df1a38fc3092d39f4acfe8289308beb8ee16
Renan Kosicki <renan@digi.ag>
2015-09-24 13:46:25 -0300

    Resources added, drawing enabled

--- swifty-ninja/swifty-ninja/GameScene.swift
+++ swifty-ninja/swifty-ninja/GameScene.swift
@@ -9,36 +9,136 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    var gameScore: SKLabelNode!
+    var livesImages = [SKSpriteNode]()
+    var lives = 3
+    var activeSliceBG: SKShapeNode!
+    var activeSliceFG: SKShapeNode!
+    var activeSlicePoints = [CGPoint]()
+    var score: Int = 0 {
+        didSet {
+            gameScore.text = "Score: \(score)"
+        }
+    }
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 45;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        let background = SKSpriteNode(imageNamed: "sliceBackground")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .Replace
+        background.zPosition = -1
+        addChild(background)
         
-        self.addChild(myLabel)
+        physicsWorld.gravity = CGVector(dx: 0, dy: -6)
+        physicsWorld.speed = 0.85
+        
+        createScore()
+        createLives()
+        createSlices()
+    }
+    
+    func createScore() {
+        gameScore = SKLabelNode(fontNamed: "Chalkduster")
+        gameScore.text = "Score: 0"
+        gameScore.horizontalAlignmentMode = .Left
+        gameScore.fontSize = 48
+        
+        addChild(gameScore)
+        
+        gameScore.position = CGPoint(x: 8, y: 8)
+    }
+    
+    func createLives() {
+        for i in 0 ..< 3 {
+            let spriteNode = SKSpriteNode(imageNamed: "sliceLife")
+            spriteNode.position = CGPoint(x: CGFloat(834 + (i * 70)), y: 720)
+            addChild(spriteNode)
+            
+            livesImages.append(spriteNode)
+        }
+    }
+    
+    func createSlices() {
+        activeSliceBG = SKShapeNode()
+        activeSliceBG.zPosition = 2
+        
+        activeSliceFG = SKShapeNode()
+        activeSliceFG.zPosition = 2
+        
+        activeSliceBG.strokeColor = UIColor(red: 1, green: 0.9, blue: 0, alpha: 1)
+        activeSliceBG.lineWidth = 9
+        
+        activeSliceFG.strokeColor = UIColor.whiteColor()
+        activeSliceFG.lineWidth = 5
+        
+        addChild(activeSliceBG)
+        addChild(activeSliceFG)
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        
+        let location = touch.locationInNode(self)
+        
+        activeSlicePoints.append(location)
+        redrawActiveSlice()
+    }
+   
+    override func touchesCancelled(touches: Set<UITouch>?, withEvent event: UIEvent?) {
+        if let touches = touches {
+            touchesEnded(touches, withEvent: event)
+        }
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
+        super.touchesBegan(touches, withEvent: event)
         
-        for touch in touches {
+        // 1
+        activeSlicePoints.removeAll(keepCapacity: true)
+        
+        // 2
+        if let touch = touches.first {
             let location = touch.locationInNode(self)
+            activeSlicePoints.append(location)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            // 3
+            redrawActiveSlice()
             
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
+            // 4
+            activeSliceBG.removeAllActions()
+            activeSliceFG.removeAllActions()
             
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+            // 5
+            activeSliceBG.alpha = 1
+            activeSliceFG.alpha = 1
         }
     }
-   
+    
+    func redrawActiveSlice() {
+        // 1
+        if activeSlicePoints.count < 2 {
+            activeSliceBG.path = nil
+            activeSliceFG.path = nil
+            return
+        }
+        
+        // 2
+        while activeSlicePoints.count > 12 {
+            activeSlicePoints.removeAtIndex(0)
+        }
+        
+        // 3
+        let path = UIBezierPath()
+        path.moveToPoint(activeSlicePoints[0])
+        
+        for var i = 1; i < activeSlicePoints.count; ++i {
+            path.addLineToPoint(activeSlicePoints[i])
+        }
+        
+        // 4
+        activeSliceBG.path = path.CGPath
+        activeSliceFG.path = path.CGPath
+    }
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
